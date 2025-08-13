82e9779bfdc916e6628d2c94ecf2b5dbb11fde71
Jolvera2001 <jolvera6@stedwards.edu>
2025-08-12 23:41:45 -0500

    stopping point

--- src/main/java/dev/jolvera/finjav/HelloController.java
+++ src/main/java/dev/jolvera/finjav/HelloController.java
@@ -2,9 +2,32 @@ package dev.jolvera.finjav;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private TextField searchField;
+
+    @FXML
+    private ListView<String> sidebarList;
+
+    @FXML
+    private void Initialize() {
+        sidebarList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    @FXML
+    private void addTransaction() {
+        System.out.println("addTransaction pressed");
+    }
+
+    public void createProjection() {
+        System.out.println("createProjection pressed");
+    }
+
+    public void refreshTransactions() {
+        System.out.println("refreshTransactions pressed");
+    }
 }
