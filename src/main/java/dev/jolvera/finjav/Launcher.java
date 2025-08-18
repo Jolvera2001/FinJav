c5eb132306fde86ea007c5d6abed013b92ab8d5a
Jolvera2001 <jolvera6@stedwards.edu>
2025-08-17 19:42:28 -0500

    More kotlin now because I hate myself

--- src/main/java/dev/jolvera/finjav/Launcher.java
+++ src/main/java/dev/jolvera/finjav/Launcher.java
@@ -2,6 +2,7 @@ package dev.jolvera.finjav;
 
 import javafx.application.Application;
 
+// TODO: Do Kotlin refactor cause why not
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
