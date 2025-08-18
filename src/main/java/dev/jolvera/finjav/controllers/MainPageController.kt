a369fdc03bffed4584d05e22fd69693a600fa4de
Jolvera2001 <jolvera6@stedwards.edu>
2025-08-18 13:53:25 -0500

    refactor mostly done

--- src/main/java/dev/jolvera/finjav/controllers/MainPageController.kt
+++ src/main/java/dev/jolvera/finjav/controllers/MainPageController.kt
@@ -1,13 +1,18 @@
 package dev.jolvera.finjav.controllers
 
+import dev.jolvera.finjav.HelloApplication
+import dev.jolvera.finjav.models.User
 import dev.jolvera.finjav.viewModels.MainViewModel
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
 import javafx.scene.control.Button
+import javafx.scene.control.Dialog
 import javafx.scene.control.Label
 import javafx.scene.control.MenuItem
 import javafx.scene.control.TextField
+import java.util.Optional
 
 class MainPageController(private val viewModel: MainViewModel) {
     @FXML private val SearchField: TextField? = null
@@ -45,6 +50,31 @@ class MainPageController(private val viewModel: MainViewModel) {
     private fun logOut() { viewModel.activeUserProperty().set(null) }
 
     private fun handleOpenAccountDialog() {
+        try {
+            val loader = FXMLLoader(HelloApplication::class.java.getResource("account-dialog.fxml"))
+            // figure out how to get injected controller for dialog within koin
+            // controller = koin get controller
+            loader.setController(controller)
+            val dialogPane = loader.load()
 
+            val dialog = Dialog<User>()
+            dialog.dialogPane = dialogPane
+            dialog.title = "Login/Register Account"
+
+            val result: Optional<User?> = dialog.showAndWait()
+
+            if (result.isPresent) {
+                viewModel.activeUserProperty().set(result.get())
+            } else {
+                val controllerResult = controller.getResult()
+                if (controllerResult != null) {
+                    viewModel.activeUserProperty().set(controllerResult)
+                } else {
+                    println("Account dialog was cancelled")
+                }
+            }
+        } catch (ex: Exception) {
+            println(ex.message)
+        }
     }
 }
